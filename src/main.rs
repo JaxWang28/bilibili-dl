3d5df6ad6cb927b62d441703ca2b82b3a40681b9
jackson <jw.jacksonwang28@gmail.com>
2023-10-08 18:30:48 +0800

    login finish

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,161 @@
 /***************************************************************************
-*
-*     Project                 ____  ____  ____  _     
-*                            | __ )| __ )|  _ \| |    
-*                            |  _ \|  _ \| | | | |    
-*                            | |_) | |_) | |_| | |___ 
-*                            |____/|____/|____/|_____|
-*
-*
-*     A commandline tool to download bilibili video.
+*                                                                          *
+*     Project                 ____  ____  ____  _                          *
+*                            | __ )| __ )|  _ \| |                         *
+*                            |  _ \|  _ \| | | | |                         *
+*                            | |_) | |_) | |_| | |___                      *
+*                            |____/|____/|____/|_____|                     *
+*                                                                          *
+*                                                                          *
+*     A commandline tool to download bilibili video.                       *
 ***************************************************************************/
+//use clap::{Parser};
+//use url::{Url, ParseError,Host, Position};
+use url::{Url};
+use reqwest;
+use serde::Deserialize;
+use serde::Serialize;
+use std::io::{self, Write};
+
+
+//use std::fs;
+//use std::collections::HashMap;
 
 
 
-use std::path::PathBuf;
 
-use clap::{Parser, Subcommand};
+#[derive(Deserialize, Serialize, Debug)]
+struct Response <T>{
+    code: i32,
+    message: String,
+    ttl: i32,
+    data: T,
+}
 
+
+/*
 #[derive(Parser)]
-#[command(author="jackson", version="0.0.1", about="A commandline tool to download bilibili video.", long_about = None)]
+#[command(author="jackson", version="0.0.1", about="A commandline program to download bilibili video.", long_about = None)]
 struct Cli {
-    /// Optional name to operate on
-    name: Option<String>,
+    /// URLs to download
+    url: Vec<String>,
+}
+*/
 
-    /// Sets a custom config file
-    #[arg(short, long, value_name = "FILE")]
-    config: Option<PathBuf>,
+fn login() -> Result<(), reqwest::Error> {
+    let cookie_store = reqwest_cookie_store::CookieStore::new(None);
+    let cookie_store = reqwest_cookie_store::CookieStoreMutex::new(cookie_store);
+    let cookie_store = std::sync::Arc::new(cookie_store);
 
-    /// Turn debugging information on
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
 
-    #[command(subcommand)]
-    command: Option<Commands>,
+    // Build a `reqwest` Client, providing the deserialized store
+    let client = reqwest::blocking::Client::builder()
+    .cookie_provider(std::sync::Arc::clone(&cookie_store))
+    .build()
+    .unwrap();
+
+    let response = client.get("https://passport.bilibili.com/x/passport-login/web/qrcode/generate").send()?;
+
+
+
+    #[derive(Deserialize,Serialize)]
+    struct Data {
+        url: String,
+        qrcode_key: String,
+    }
+
+
+    let response:Response<Data>= response.json()?;
+    println!("{}", response.data.url);
+
+
+    let mut input = String::new();
+    loop {
+        input.clear();  // Clear the previous input
+
+        print!("Continue? (y/N): ");
+        io::stdout().flush().unwrap();  // Flush to ensure the prompt is displayed before waiting for input
+
+        io::stdin().read_line(&mut input).unwrap();
+
+        if input.trim().eq_ignore_ascii_case("y") {
+            println!("Continuing...");
+            break;  // Exit the loop
+        } else {
+            println!("Invalid input. Please enter 'y' or 'Y'.");
+        }
+    }
+
+
+    // 解析 URL
+    let mut url = Url::parse("https://passport.bilibili.com/x/passport-login/web/qrcode/poll").expect("Failed to parse URL");
+
+    // 添加查询参数
+    url.query_pairs_mut()
+        .append_pair("qrcode_key", &response.data.qrcode_key);
+
+    //println!("{:#?}", url.to_string());
+    let _response = client.get(&url.to_string()).send()?;
+
+    {
+      // Write store back to disk
+      let mut writer = std::fs::File::create("cookies.json")
+          .map(std::io::BufWriter::new)
+          .unwrap();
+      let store = cookie_store.lock().unwrap();
+      store.save_json(&mut writer).unwrap();
+    }
+    Ok(())
 }
 
-#[derive(Subcommand)]
-enum Commands {
-    /// does testing things
-    Test {
-        /// lists test values
-        #[arg(short, long)]
-        list: bool,
-    },
-    /// download video
-    Download {
 
-    },
-    /// login 
-    Login {
 
-    },
+
+
+fn login_test() -> Result<(), reqwest::Error> {
+    // Load an existing set of cookies, serialized as json
+    let cookie_store = {
+      if let Ok(file) = std::fs::File::open("cookies.json")
+        .map(std::io::BufReader::new)
+        {
+          // use re-exported version of `CookieStore` for crate compatibility
+          reqwest_cookie_store::CookieStore::load_json(file).unwrap()
+        }
+        else
+        {
+          reqwest_cookie_store::CookieStore::new(None)
+        }
+    };
+    let cookie_store = reqwest_cookie_store::CookieStoreMutex::new(cookie_store);
+    let cookie_store = std::sync::Arc::new(cookie_store);
+    {
+      // Examine initial contents
+      println!("initial load");
+      let store = cookie_store.lock().unwrap();
+      for c in store.iter_any() {
+        println!("{:?}", c);
+      }
+    }
+
+
+    // Build a `reqwest` Client, providing the deserialized store
+    let client = reqwest::blocking::Client::builder()
+    .cookie_provider(std::sync::Arc::clone(&cookie_store))
+    .build()
+    .unwrap();
+    let response = client.get("https://api.bilibili.com/x/web-interface/nav").send()?;
+    let body = response.text();
+    println!("{:#?}", body);
+
+    Ok(())
+
 }
 
-fn main() {
-    let cli = Cli::parse();
+fn main(){
+    //let cli = Cli::parse();
 
     // You can check the value provided by positional arguments, or option arguments
+    /*
     if let Some(name) = cli.name.as_deref() {
         println!("Value for name: {name}");
     }
@@ -63,39 +163,40 @@ fn main() {
     if let Some(config_path) = cli.config.as_deref() {
         println!("Value for config: {}", config_path.display());
     }
+    */
 
-    // You can see how many times a particular flag or argument occurred
-    // Note, only flags can have multiple occurrences
-    match cli.debug {
-        0 => println!("Debug mode is off"),
-        1 => println!("Debug mode is kind of on"),
-        2 => println!("Debug mode is on"),
-        _ => println!("Don't be crazy"),
+    //assert!(Url::parse("http://[:asdfas::1]") == Err(ParseError::InvalidIpv6Address));
+    /*
+    println!("url: {:?}", cli.url);
+    for item in &cli.url {
+        println!("{:?}", item);
+        let issue_list_url = Url::parse(item);
+        println!("{:?}", issue_list_url);
     }
+    */
 
-    // You can check for the existence of subcommands, and if found use their
-    // matches just as you would the top level cmd
-    match &cli.command {
-        Some(Commands::Test { list }) => {
-            if *list {
-                println!("Printing testing lists...");
-            } else {
-                println!("Not printing testing lists...");
-            }
-        }
-        Some(Commands::Download { }) => {
-            println!("Printing testing lists...");
-            println!("Not printing testing lists...");
-        }
-        Some(Commands::Login { }) => {
-            println!("Printing testing lists...");
-            println!("Not printing testing lists...");
-        }
-        
-        None => {}
-    }
+    let _ = login();
+    //let _ = login_test();
 
-    // Continued program logic goes here...
+    /*
+    assert!(issue_list_url.scheme() == "https");
+    assert!(issue_list_url.username() == "");
+    assert!(issue_list_url.password() == None);
+    assert!(issue_list_url.host_str() == Some("github.com"));
+    assert!(issue_list_url.host() == Some(Host::Domain("github.com")));
+    assert!(issue_list_url.port() == None);
+    assert!(issue_list_url.path() == "/rust-lang/rust/issues");
+    assert!(issue_list_url.path_segments().map(|c| c.collect::<Vec<_>>()) ==
+            Some(vec!["rust-lang", "rust", "issues"]));
+    assert!(issue_list_url.query() == Some("labels=E-easy&state=open"));
+    assert!(&issue_list_url[Position::BeforePath..] == "/rust-lang/rust/issues?labels=E-easy&state=open");
+    assert!(issue_list_url.fragment() == None);
+    assert!(!issue_list_url.cannot_be_a_base());
+    */
+
+
+    
+
 }
 
 
@@ -120,4 +221,39 @@ fn print_help() {
     println!("  login                login in account");
     println!("  download             download,default");
 }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+    //save_cookies_to_file(&response, "./cookies.txt");
+    //let theheader = response.headers();
+    //println!("{:#?}", theheader);
+    //let cookies = theheader.get_all("set-cookie");
+
+    //println!("{:#?}",cookies);
+
+
+    /*
+    let mut iter = cookies.iter();
+    loop {
+        println!("{:#?}", iter.next().unwrap());
+        if iter.next().is_none() {
+            break;
+        }
+    }
+    */
+    //let body = response.text();
+    //println!("{:#?}", body);
+
 */
