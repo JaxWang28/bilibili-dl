6c33eb45f68f0e01145627e288c9e83563e1b136
jackson <jw.jacksonwang28@gmail.com>
2023-10-09 18:37:47 +0800

    version 0.0.0.1, before relase

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,7 @@ enum Commands {
     },
     /// download video
     Download {
+        url: String
 
     }
 }
@@ -123,14 +124,66 @@ fn login() -> Result<(), reqwest::Error> {
 
 
 
+struct video{
+    bvid: String, 
+    cid: i32,
+}
+
+#[derive(Deserialize,Serialize, Debug)]
+struct Obj {
+    id:i32,
+    baseUrl:String,
+    base_url:String,
+}
 
 
+#[derive(Deserialize,Serialize, Debug)]
+struct Dash {
+    video: Vec<Obj>,
+    audio: Vec<Obj>,
+}
 
 
+fn fetchstream_core(mut response: reqwest::blocking::Response, client: &reqwest::blocking::Client, name: &str) {
+    let mut size: u64 = 0;
+    match response.headers().get(CONTENT_LENGTH) {
+        Some(length) => {
+            size = length.to_str().unwrap().parse().unwrap();
+            println!("File size: {} bytes", size);
+        }
+        None => println!("Content-Length header is missing!"),
+    }
 
-/*
-fn login_test() -> Result<(), reqwest::Error> {
-    // Load an existing set of cookies, serialized as json
+
+    let mut file = OpenOptions::new().create(true).append(true).open(name).unwrap();
+    
+    let mut num = 0;
+    let mut current_offset = file.seek(SeekFrom::Current(0)).unwrap();
+    println!("Current offset: {}", current_offset);
+
+    
+    loop {
+        let range = format!("bytes={}-", current_offset);
+        println!("{}", range);
+        let mut response = client.get(response.url().to_string()).header(RANGE, range).send().unwrap();
+
+        let result = copy(&mut response, &mut file);
+        match result {
+            Ok(value) => {
+                println!("ok");
+                break;
+            },
+            Err(err) => {
+                println!("error: {:#?}", err);
+                // handle the error case
+            },
+        }
+        current_offset = file.seek(SeekFrom::Current(0)).unwrap();
+        println!("Current offset: {}", current_offset);
+    }
+}
+
+fn fetchstream(dash:Dash) -> i32{
     let cookie_store = {
       if let Ok(file) = std::fs::File::open("cookies.json")
         .map(std::io::BufReader::new)
@@ -145,14 +198,7 @@ fn login_test() -> Result<(), reqwest::Error> {
     };
     let cookie_store = reqwest_cookie_store::CookieStoreMutex::new(cookie_store);
     let cookie_store = std::sync::Arc::new(cookie_store);
-    {
-      // Examine initial contents
-      println!("initial load");
-      let store = cookie_store.lock().unwrap();
-      for c in store.iter_any() {
-        println!("{:?}", c);
-      }
-    }
+
 
 
     // Build a `reqwest` Client, providing the deserialized store
@@ -160,14 +206,168 @@ fn login_test() -> Result<(), reqwest::Error> {
     .cookie_provider(std::sync::Arc::clone(&cookie_store))
     .build()
     .unwrap();
-    let response = client.get("https://api.bilibili.com/x/web-interface/nav").send()?;
-    let body = response.text();
-    println!("{:#?}", body);
 
-    Ok(())
+    let mut response_result: Option<reqwest::blocking::Response> = None;
 
+    for video in &dash.video {
+        response_result = Some(client.get(&video.baseUrl).send().unwrap());
+        //println!("{:?}", video.baseUrl);
+        match &response_result {
+            Some(response) => {
+                if response.status() == 200 {
+                    println!("403");
+                    break;
+                }
+            }
+            None => return 0,
+        }
+    }
+    let mut response: reqwest::blocking::Response = response_result.unwrap();
+    fetchstream_core(response, &client, "file.mp4");
+
+    let mut response_result: Option<reqwest::blocking::Response> = None;
+
+    for audio in &dash.audio {
+        response_result = Some(client.get(&audio.baseUrl).send().unwrap());
+        //println!("{:?}", video.baseUrl);
+        match &response_result {
+            Some(response) => {
+                if response.status() == 200 {
+                    println!("403");
+                    break;
+                }
+            }
+            None => return 0,
+        }
+    }
+    let mut response: reqwest::blocking::Response = response_result.unwrap();
+    fetchstream_core(response, &client, "file.mp3");
+    0
 }
-*/
+
+fn download(url: &String){
+    let dash = getstream(url);
+    //println!("{:#?}",dash);
+    fetchstream(dash);
+    let result = mix_audio_video("file.mp4", "file.mp3", "outputfile.mp4");
+}
+
+
+use std::process::Command;
+
+fn mix_audio_video(video_path: &str, audio_path: &str, output_path: &str) -> std::io::Result<()> {
+    let status = Command::new("ffmpeg")
+        .arg("-i")
+        .arg(video_path)
+        .arg("-i")
+        .arg(audio_path)
+        .arg("-c:v")
+        .arg("copy")
+        .arg("-c:a")
+        .arg("aac")
+        .arg("-strict")
+        .arg("experimental")
+        .arg(output_path)
+        .status()?;
+
+    if status.success() {
+        Ok(())
+    } else {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Failed to mix audio and video",
+        ))
+    }
+}
+
+fn getstream(url: &String) -> Dash{
+    let mut url = Url::parse(url).expect("Failed to parse URL");
+
+    let prefix = "/video/";
+    let suffix = "/";
+    let path = url.path();
+    let start = prefix.len();
+    let end = path.len() - suffix.len();
+    let bvid = &path[start..end];
+    println!("{}", bvid);
+
+    let cookie_store = {
+      if let Ok(file) = std::fs::File::open("cookies.json")
+        .map(std::io::BufReader::new)
+        {
+          // use re-exported version of `CookieStore` for crate compatibility
+          reqwest_cookie_store::CookieStore::load_json(file).unwrap()
+        }
+        else
+        {
+          reqwest_cookie_store::CookieStore::new(None)
+        }
+    };
+    let cookie_store = reqwest_cookie_store::CookieStoreMutex::new(cookie_store);
+    let cookie_store = std::sync::Arc::new(cookie_store);
+
+
+    // Build a `reqwest` Client, providing the deserialized store
+    let client = reqwest::blocking::Client::builder()
+    .cookie_provider(std::sync::Arc::clone(&cookie_store))
+    .build()
+    .unwrap();
+
+    let mut url = Url::parse("https://api.bilibili.com/x/web-interface/view").expect("Failed to parse URL");
+
+    
+    url.query_pairs_mut()
+        .append_pair("bvid", &bvid);
+
+    //println!("{:#?}", url.to_string());
+    let response = client.get(&url.to_string()).send().unwrap();
+    #[derive(Deserialize,Serialize, Debug)]
+    struct Data {
+        bvid: String,
+        title: String,
+        cid: i32,
+    }
+    let response:Response<Data>= response.json().unwrap();
+    println!("{:#?}", response.data);
+
+
+    let mut url = Url::parse("https://api.bilibili.com/x/player/playurl").expect("Failed to parse URL");
+
+    //let mut url = Url::parse("https://api.bilibili.com/x/player/wbi/playurl").expect("Failed to parse URL");
+    url.query_pairs_mut()
+        .append_pair("bvid", &bvid);
+    url.query_pairs_mut()
+        .append_pair("cid", &response.data.cid.to_string());
+    url.query_pairs_mut()
+        .append_pair("fnval", "16");
+
+    let response = client.get(&url.to_string()).send().unwrap();
+    #[derive(Deserialize,Serialize, Debug)]
+    struct Data2 {
+        accept_description: Vec<String>,
+        dash:Dash,
+    }
+    let response:Response<Data2>= response.json().unwrap();
+    println!("{:#?}", response);
+    response.data.dash
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn exploration() {
+        getVideoInfo();
+    }
+}
+use std::fs::File;
+use std::io::copy;
+use reqwest::header::CONTENT_LENGTH;
+use reqwest::header::RANGE;
+use std::fs::OpenOptions;
+use std::io::{Seek, SeekFrom};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
@@ -178,8 +378,11 @@ fn main() -> Result<(), Box<dyn Error>> {
             let _ = login();
             return Ok(());
         }
-        Commands::Download {} => {
+        Commands::Download {url} => {
             println!("download video");
+            //getVideoInfo();
+            //downloadVideo();
+            download(url);
             return Ok(());
         }
     }
@@ -277,3 +480,47 @@ fn print_help() {
     //println!("{:#?}", body);
 
 */
+
+
+
+/*
+fn login_test() -> Result<(), reqwest::Error> {
+    // Load an existing set of cookies, serialized as json
+    let cookie_store = {
+      if let Ok(file) = std::fs::File::open("cookies.json")
+        .map(std::io::BufReader::new)
+        {
+          // use re-exported version of `CookieStore` for crate compatibility
+          reqwest_cookie_store::CookieStore::load_json(file).unwrap()
+        }
+        else
+        {
+          reqwest_cookie_store::CookieStore::new(None)
+        }
+    };
+    let cookie_store = reqwest_cookie_store::CookieStoreMutex::new(cookie_store);
+    let cookie_store = std::sync::Arc::new(cookie_store);
+    {
+      // Examine initial contents
+      println!("initial load");
+      let store = cookie_store.lock().unwrap();
+      for c in store.iter_any() {
+        println!("{:?}", c);
+      }
+    }
+
+
+    // Build a `reqwest` Client, providing the deserialized store
+    let client = reqwest::blocking::Client::builder()
+    .cookie_provider(std::sync::Arc::clone(&cookie_store))
+    .build()
+    .unwrap();
+    let response = client.get("https://api.bilibili.com/x/web-interface/nav").send()?;
+    let body = response.text();
+    println!("{:#?}", body);
+
+    Ok(())
+
+}
+*/
+
